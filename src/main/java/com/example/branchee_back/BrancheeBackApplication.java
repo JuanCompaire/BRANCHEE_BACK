c641f0dd4ebd14b21b3a0e58957d1478198b6533
JuanCompaire <117914634+JuanCompaire@users.noreply.github.com>
2024-06-20 13:28:11 +0200

    START PROYECT

---
+++ src/main/java/com/example/branchee_back/BrancheeBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.branchee_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrancheeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrancheeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/branchee_back/BrancheeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.branchee_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrancheeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
