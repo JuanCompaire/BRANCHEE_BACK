4ed9834dbdc3f551de30055146c14489b3122657
JuanCompaire <117914634+JuanCompaire@users.noreply.github.com>
2024-07-19 14:13:44 +0200

    signup funcional

--- src/main/java/com/example/branchee_back/controller/signup/SignUpController.java
+++ src/main/java/com/example/branchee_back/controller/signup/SignUpController.java
@@ -3,23 +3,28 @@ package com.example.branchee_back.controller.signup;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.branchee_back.entity.Usuario;
+import com.example.branchee_back.service.SignUpService;
 
 @RestController
+@RequestMapping("/signup")
 public class SignUpController {
 
-    @PostMapping("/signup")
-    public ResponseEntity<Map<String, String>> signUp(@RequestBody Usuario user) {
-        // Implementa la lógica para procesar el usuario recibido
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Usuario registrado correctamente");
-        System.out.println("SIGNUP :"+ user);
-        return ResponseEntity.ok(response);
+    @Autowired
+    private SignUpService service;
+
+    @PostMapping
+    public void signUpUser(@RequestBody Usuario user){
+        service.SignUpUser(user);
+        System.out.println("SIGNUP : "+ user);
     }
     
 }
