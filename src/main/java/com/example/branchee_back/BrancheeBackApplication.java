06ba0beecc9ce80126be0c8d6a1ed603ea952eab
JuanCompaire <117914634+JuanCompaire@users.noreply.github.com>
2024-07-19 12:29:24 +0200

    ahora escucha tanto a front end desde fuera como desde dentro(dist)

--- src/main/java/com/example/branchee_back/BrancheeBackApplication.java
+++ src/main/java/com/example/branchee_back/BrancheeBackApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin(origins = "http://localhost:4200") // Habilitar CORS globalmente
+//@CrossOrigin(origins = "http://localhost:4200") // Habilitar CORS globalmente
 public class BrancheeBackApplication {
 
 	public static void main(String[] args) {
